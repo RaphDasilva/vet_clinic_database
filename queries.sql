10c9c3d6faa47681ee42dc6b947f6b2f502195dc
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-29 20:10:21 +0530

    Add query to list all animals owned by Dean Winchester that haven't tried to escape.

--- queries.sql
+++ queries.sql
@@ -104,3 +104,9 @@ SELECT owner_id, full_name AS "OWNER NAME" , animals.name AS "ANIMALS NAME"
    INNER JOIN owners ON  animals.owner_id = owners.id
    INNER JOIN species ON  animals.species_id = species.id
    WHERE owners.id = 2 and species.name = 'Digimon';
+
+   SELECT full_name AS "OWNER NAME" , animals.name AS "ANIMALS NAME",species.name AS "Animal Species"
+   FROM animals
+   INNER JOIN owners ON  animals.owner_id = owners.id
+   INNER JOIN species ON  animals.species_id = species.id
+   WHERE owners.id = 5 and animals.escape_attempts = 0;  
