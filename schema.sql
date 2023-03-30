6d7d9b3e595355ac72b7a6fc34de7f2d18c8d995
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-30 11:42:09 +0100

    Create a table named vets.

--- schema.sql
+++ schema.sql
@@ -47,3 +47,12 @@ ADD CONSTRAINT fk_owner
 FOREIGN KEY(owner_id)
 REFERENCES owners(id);
 
+-- many-to-many
+
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY (id)
+);
