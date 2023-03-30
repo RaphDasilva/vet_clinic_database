b1e1343d655e3ece26a4cca5fb9356b303fa3d68
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-30 15:04:18 +0100

    How many visits were with a vet that did not specialize in that animal's species?

--- queries.sql
+++ queries.sql
@@ -164,6 +164,12 @@ SELECT owner_id, full_name AS "OWNER NAME" , animals.name AS "ANIMALS NAME"
     JOIN vets ON visits.vets_id = vets.id
     ORDER BY visit_date DESC LIMIT 1;
 
+     SELECT COUNT(*) AS "number of visits" FROM vets
+    INNER JOIN visits ON vets.id = visits.vets_id
+    LEFT JOIN specializations ON vets.id = specializations.vets_id
+    LEFT JOIN species ON specializations.species_id = species.id
+    WHERE species IS NULL;
+
     SELECT species.name AS "Species Name" 
     FROM visits
     JOIN animals ON visits.animal_id = animals.id 
@@ -172,3 +178,5 @@ SELECT owner_id, full_name AS "OWNER NAME" , animals.name AS "ANIMALS NAME"
     WHERE visits.vets_id = (SELECT id FROM vets WHERE name = 'Maisy Smith') 
     GROUP BY species.name 
     ORDER BY count(animals.name) DESC LIMIT 1;
+
+   
